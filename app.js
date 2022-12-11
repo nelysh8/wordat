ec32b31ef50c43c1536d157d581d2634e192661c
unknown <nelysh8@gmail.com>
2022-12-11 12:55:47 +0900

    make it better

--- app.js
+++ app.js
@@ -54,8 +54,7 @@ app.use(express.static('public'));
 // my image folder
 
 app.get('/images/:img_name', function(req,res){
-  var img_name = req.params.img_name;  
-  
+  var img_name = req.params.img_name;
   fs.readFile('/images/icon_google.png',function(err, data) {
     // res.send(data);
     res.setHeader('Content-Type', 'image/jpg');
@@ -70,20 +69,27 @@ app.get('/images/:img_name', function(req,res){
 
 // papago
 
-
 app.post('/translate', function (req, res) {
+
+  console.log('papago launch1');
 
   var client_id = 'jfubHYsSNbt90eWAe_lY';  // 'YOUR_CLIENT_ID';
   var client_secret = 'k3g0N0ERA6';  // YOUR_CLIENT_SECRET';
 
   var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
   var query = req.body.word;  
+
+  console.log(query);
+
   var request = require('request');
   var options = {
     url: api_url,
     form: {'source':'en', 'target':'ko', 'text':query},
     headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
   };
+  
+  console.log('papago launch2');
+
   request.post(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
@@ -94,6 +100,13 @@ app.post('/translate', function (req, res) {
     }
   });
 });
+
+// app.listen(3000, function () {
+//   console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
+// });
+
+
+
 
 // websearch_cambrg
 
@@ -116,39 +129,119 @@ app.post('/websearch_cambrg', function (req, res) {
       let whList = [];
       let ulList = [];
       const $ = cheerio.load(html.data);      
-      const $wordhead = $('#page-content > div.pr.entry-body > div.di.english-korean > div > span > div:nth-child(1) > span > div');
-      const $pronounce = $('#page-content > div.pr.entry-body > div.di.english-korean > div > span > div:nth-child(1) > span > div > span:nth-child(5)');
+      const $wordhead = $('div.di-body');      
+      // console.log('hi : ' + $wordhead.find('div.entry-body__el').length);
+      // console.log('next : ' + $wordhead.find('div.entry-body__el').html());     
+      let i = 0;
 
-      whList = {
-        word : $wordhead.find('div.di-title h2.headword span').text(),
-        part : $wordhead.find('span.pos').text(),
-        pronounce : $pronounce.find('span.ipa').text(),
-        pronounce_link : $pronounce.find('span.us source').attr('src')
+      for (let headhtml of $wordhead.find('div.entry-body__el')) {        
+        const $head = $wordhead.find(`div.entry-body__el:nth-child(${i+1})`);
+        
+        whList[i] = {
+          word : $head.find('div.di-title h2.headword span').text(),
+          part : $head.find('span.pos').text(),         
+          pronounce : $head.find('span.us').parent().find('span.ipa').text(),
+          pronounce_link : $head.find('span.us source').attr('src')
+        };                
+
+        // console.log('hi : ' + $head.find('div.sense-block').length);
+        
+        let j = 0;
+        ulList = [];
+
+        for (let meaningbox of $head.find('div.sense-block')) {
+          const $meaningline = $head.find(`div.sense-block:nth-child(${j+1})`);          
+
+          ulList[j] = {
+                meaning_eng : $meaningline.find('div.ddef_d').text().trim(),                  
+                meaning_kor : $meaningline.find('span.trans').text().trim(),
+                example : [$meaningline.find('div.def-body div.examp:nth-child(2) span').text().trim(), $meaningline.find('div.def-body div.examp:nth-child(3) span').text().trim()]                
+          };                    
+          j += 1;        
+        };  
+        
+        // console.log(ulList);
+        whList[i].meanings = ulList; 
+        // console.log('-------------WL-----------------' + whList);
+        i += 1;
       };
+      // console.log('next : ' + $wordhead.find('div.entry-body__el').html());     
+    const data = whList;    
+    return data;
+    })
 
-      console.log($wordhead.find('div.di-title h2.headword span').html());
-      console.log(whList);
+.then(result => {      
+  // console.log(result);          
+  res.json(result);
+});
 
-      const $bodyList = $('#page-content > div.pr.entry-body > div.di.english-korean > div > span > div:nth-child(1)').children('div.pos-body').children('div.sense-block');
+});
+
+        // $bodyList.each(function(i, elem) {          
+        //   ulList[i] = {
+        //     meaning_eng : $(this).find('div.ddef_d').text().trim(),  
+        //     // meaning_eng : $(this).find('div.ddef_d').text().trim(),
+        //     meaning_kor : $(this).find('span.trans').text().trim(),
+        //     example : [$(this).find('div.examp:nth-child(2) span').text().trim(), $(this).find('div.examp:nth-child(3) span').text().trim()]
+
+
+
+
+
+        // const $bodyList = $('#page-content > div.pr.entry-body > div.di.english-korean > div > span > div:nth-child(1)').children('div.pos-body').children('div.sense-block');
       // console.log($bodyList.find('div.ddef_d').text());
       // const $bodyList = $("div.entry-body").children("entry-body__el");
       // const $bodyList = $("div.pos-body").children("ul").children("li");      
       // console.log($wordhead.find('h2.headword').text());
-      $bodyList.each(function(i, elem) {          
-          ulList[i] = {
-            meaning_eng : $(this).find('div.ddef_d').text().trim(),  
-            // meaning_eng : $(this).find('div.ddef_d').text().trim(),
-            meaning_kor : $(this).find('span.trans').text().trim(),
-            example : [$(this).find('div.examp:nth-child(2) span').text().trim(), $(this).find('div.examp:nth-child(3) span').text().trim()]
+      
 
               // url: 'search.naver.com/search.naver'+$(this).find('div.list_title a').attr('href'),
               // image_url: $(this).find('div.list_thumb a img').attr('src'),
               // image_alt: $(this).find('div.list_thumb a img').attr('alt'),
-      };
-      });
+      
 
-      whList.meanings = ulList;
-      const data = whList;
+      // console.log(whList);
+      
+      // $wordhead.each(function(i, elem) {          
+        
+        // console.log(this.find('h2.headword').length);
+        // const $pronounce = $wordhead(`span.us:nth-child(${i+1})`).parent();
+        // console.log($pronounce.html());
+        
+        // whList[i] = {
+        //   word : $wordhead.find(`div.di-title h2.headword:nth-child(${i+1}) span`).text(),
+        //   part : $wordhead.find(`span.pos:nth-child(${i+1})`).text(),
+          // pronounce : $pronounce.find('span.ipa').text(),
+          // pronounce_link : $pronounce.find('span.us source').attr('src')
+      //   };
+      //   console.log(whList);
+      // });
+
+
+
+    //       meaning_eng : $(this).find('div.ddef_d').text().trim(),  
+    //       // meaning_eng : $(this).find('div.ddef_d').text().trim(),
+    //       meaning_kor : $(this).find('span.trans').text().trim(),
+    //       example : [$(this).find('div.examp:nth-child(2) span').text().trim(), $(this).find('div.examp:nth-child(3) span').text().trim()]
+
+    //         // url: 'search.naver.com/search.naver'+$(this).find('div.list_title a').attr('href'),
+    //         // image_url: $(this).find('div.list_thumb a img').attr('src'),
+    //         // image_alt: $(this).find('div.list_thumb a img').attr('alt'),
+    // };
+    // // });     
+
+    //   whList = {
+    //     word : $wordhead.find('div.di-title h2.headword span').text(),
+    //     part : $wordhead.find('span.pos').text(),
+    //     pronounce : $pronounce.find('span.ipa').text(),
+    //     pronounce_link : $pronounce.find('span.us source').attr('src')
+    //   };
+
+      // console.log($wordhead.find('div.di-title h2.headword span').html());
+      // console.log(whList);
+
+      
+      
 
       // console.log(whList);
 
@@ -156,13 +249,7 @@ app.post('/websearch_cambrg', function (req, res) {
 
       // const data = ulList;
       // const data = ulList.filter(n => n.title);
-      return data;
-  })
-  .then(result => {      
-    console.log(result);          
-    res.json(result);
-  });
-});
+ 
 
 
 //
@@ -202,9 +289,7 @@ async function quickStart() {
 quickStart();
 */
 
-// app.listen(3000, function () {
-//   console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
-// });
+
 
 
 // app.listen(3000);
